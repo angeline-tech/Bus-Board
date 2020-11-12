072160a8f1616547edb8e31b681634a9d3eb32c6
Jordan <jordan.angeline1998@gmail.com>
2020-11-12 14:11:42 +0000

    Bus Board setup

---
+++ bus_board_app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import Flask, render_template, request, redirect
+
+
+from bus_board_app.flask_config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/heartbeat')
+def index():
+    return 'Hello World!'
+
+@app.route('/')
+def home():
+    return render_template('index.html',data={})
+
+
+if __name__ == '__main__':
+    app.run()
